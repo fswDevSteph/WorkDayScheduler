9367950b1a7f03a07db1d6d60d8bd0a82b0058cd
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2023-12-01 17:02:50 -0400

    completed displaying the current date at the top of the Work Day Scheduler

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,24 @@
+/*Requirements:
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+*/
+var today = dayjs(); //Creates a variable"var" named"today" & sets"=" it to the result of calling"()" the dayjs method"function" provided by using the dayjs library.
+var formatDate = today.format('MMMM D, YYYY'); //Creates a variable"formatDate" & sets"=" it to calling() the format method"function" on the today variable and passes it the formatting style argument"('MMMM D, YYYY')
+
+document.getElementById('currentDay').textContent = formatDate; // Grabs the HTML element with the id 'currentDay' and updates its text content with the formatted date stored in the variable "formatDate".
+
+/*HEN I scroll down
+THEN I am presented with timeblocks for standard business hours of 9am&ndash;5pm
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist */
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
